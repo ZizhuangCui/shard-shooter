1e05dd37e7fedb2eb92575e5e70b2d217f999955
Jichao-tj <75312573+Jichao-tj@users.noreply.github.com>
2025-03-21 21:34:46 +0100

    add sound

--- 1.3.0 - Horizons/Shard/ConsoleApp1/obj/Debug/net8.0-windows/Shard.AssemblyInfo.cs
+++ 1.3.0 - Horizons/Shard/ConsoleApp1/obj/Debug/net8.0-windows/Shard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f1d18100e64f342a732a39e13ee8cedb8886cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01bca794d38a1c810070dc9eff1ca0f6645d9ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
